93c9e7de4b48618127f7901f4c4a676e6251fbe1
Umaima724 <umaimamubashar19@gmail.com>
2026-02-28 21:57:03 +0500

    Added A* algorithm

--- Project/algorithms.py
+++ Project/algorithms.py
@@ -18,3 +18,33 @@ def reconstruct_path(current):
         path.append(current)
         current = current.parent
     return path
+def a_star(grid, start, goal, heuristic):
+    count = 0
+    open_set = []
+    heapq.heappush(open_set, (0, count, start))
+
+    start.g = 0
+    start.f = heuristic(start, goal)
+
+    visited_nodes = []
+
+    while open_set:
+        current = heapq.heappop(open_set)[2]
+        visited_nodes.append(current)
+
+        if current == goal:
+            return reconstruct_path(goal), visited_nodes
+
+        for neighbor in grid.get_neighbors(current):
+            temp_g = current.g + 1
+
+            if temp_g < neighbor.g:
+                neighbor.parent = current
+                neighbor.g = temp_g
+                neighbor.h = heuristic(neighbor, goal)
+                neighbor.f = neighbor.g + neighbor.h
+
+                count += 1
+                heapq.heappush(open_set, (neighbor.f, count, neighbor))
+
+    return None, visited_nodes
