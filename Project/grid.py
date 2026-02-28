127c87f068048feb3eb82221f4114ca9cc51e9b8
Umaima724 <umaimamubashar19@gmail.com>
2026-02-28 22:10:04 +0500

    Update grid.py

--- Project/grid.py
+++ Project/grid.py
@@ -1,24 +1,7 @@
 # grid.py
-class Node:
-    def __init__(self, row, col):
-        self.row = row
-        self.col = col
 
-        self.is_wall = False
-        self.is_start = False
-        self.is_goal = False
-
-        # For algorithms (later)
-        self.g = float("inf")
-        self.h = 0
-        self.f = float("inf")
-        self.parent = None
-
-    def reset(self):
-        self.g = float("inf")
-        self.h = 0
-        self.f = float("inf")
-        self.parent = None
+import random
+from node import Node
 
 
 class Grid:
@@ -28,23 +11,27 @@ class Grid:
 
         self.grid = [[Node(r, c) for c in range(cols)] for r in range(rows)]
 
-        self.start = None
-        self.goal = None
+        self.start = self.grid[0][0]
+        self.goal = self.grid[rows - 1][cols - 1]
 
-    def set_start(self, row, col):
-        node = self.grid[row][col]
-        node.is_start = True
-        self.start = node
+        self.start.is_start = True
+        self.goal.is_goal = True
 
-    def set_goal(self, row, col):
-        node = self.grid[row][col]
-        node.is_goal = True
-        self.goal = node
+    def reset_algorithm(self):
+        for row in self.grid:
+            for node in row:
+                node.reset_algorithm()
 
     def toggle_wall(self, row, col):
         node = self.grid[row][col]
         if not node.is_start and not node.is_goal:
             node.is_wall = not node.is_wall
+
+    def generate_random_obstacles(self, density):
+        for row in self.grid:
+            for node in row:
+                if not node.is_start and not node.is_goal:
+                    node.is_wall = random.random() < density
 
     def get_neighbors(self, node):
         neighbors = []
@@ -55,7 +42,8 @@ class Grid:
             c = node.col + dc
 
             if 0 <= r < self.rows and 0 <= c < self.cols:
-                if not self.grid[r][c].is_wall:
-                    neighbors.append(self.grid[r][c])
+                neighbor = self.grid[r][c]
+                if not neighbor.is_wall:
+                    neighbors.append(neighbor)
 
         return neighbors
